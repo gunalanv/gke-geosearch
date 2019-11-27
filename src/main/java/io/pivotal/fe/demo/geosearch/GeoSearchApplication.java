e6a56c90a3105e42727d9b65af4331bd84de7a81
gunav <gunav@192.168.1.44>
2019-11-27 17:27:46 -0600

    Updated Class Name and Entry

--- src/main/java/io/pivotal/fe/demo/geosearch/GeoSearchApplication.java
+++ src/main/java/io/pivotal/fe/demo/geosearch/GeoSearchApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GeobsearchApplication {
+public class GeoSearchApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GeobsearchApplication.class, args);
+		SpringApplication.run(GeoSearchApplication.class, args);
 	}
 }
